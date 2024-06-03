f77c7a48a495cdd53d98efd5ef25d465d4dacfa8
underwork <lenamquoc1@gmail.com>
2024-06-03 19:27:34 +0700

    Calculate the Cos and Sin of inputted degree

---
+++ Exercise6/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercise6
+{
+    /// <summary>
+    /// Calculate and display the cosine and sine of an angle that are prompted
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float angle;
+            //Prompt and get angle
+            Console.WriteLine("Angle(Degree): ");
+            angle = float.Parse(Console.ReadLine());
+            //Convert angle from degree to radian to work with Mathf method
+            angle = angle * MathF.PI / 180;
+            //Print the calculated sine
+            Console.WriteLine("Sine: " + MathF.Sin(angle));
+            //Print the calculated cosine
+            Console.WriteLine("Cosine: " + MathF.Cos(angle));
+        }
+    }
+}
