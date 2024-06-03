073f07c418ec583135e40ae44e135f3338196ece
underwork <lenamquoc1@gmail.com>
2024-06-03 17:15:20 +0700

    Add files via upload

---
+++ Exercise3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 21;
+            Console.WriteLine(age);
+        }
+    }
+}
---
+++ Exercise4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise4
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Prompt for 2 altitude then calculate the distance it took to get from point 1 to 2
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //Get first altitude
+            Console.WriteLine("Starting point:");
+            int firstAltitude = int.Parse(Console.ReadLine());
+            //Get second altitude
+            Console.WriteLine("Stopping point:");
+            int secondAltitude = int.Parse(Console.ReadLine());
+            //Calculate and print distance
+            Console.WriteLine(secondAltitude - firstAltitude);
+
+
+        }
+    }
+}
