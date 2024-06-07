f76a4d8b7451bceea85cf438ade203461e3f3352
underwork <lenamquoc1@gmail.com>
2024-06-07 19:08:52 +0700

    Class and method manipulation with documentation
    Create a Deck object, Shuffle it and take out the two top card.

---
+++ Exercise9/Exercise9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise9
+{
+    /// <summary>
+    /// Exercise 9 solution 
+    /// </summary>
+
+    internal class Program
+    {
+        /// <summary>
+        /// Demonstrates using custom classes
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            // create a new deck and tell the deck to print itself
+            Deck deck = new Deck();
+            deck.Print();
+            Console.WriteLine("---------------------------------------------");
+            // tell the deck to shuffle and print itself
+            deck.Shuffle();
+            deck.Print();
+            Console.WriteLine("---------------------------------------------");
+            // take the top card from the deck and print the card rank and suit
+            Card card1 = deck.TakeTopCard();
+            Console.WriteLine(card1.Rank + "of" + card1.Suit);
+            Console.WriteLine("---------------------------------------------");
+            // take the top card from the deck and print the card rank and suit
+            Card card2 = deck.TakeTopCard();
+            Console.WriteLine(card2.Rank + "of" + card2.Suit);
+        }
+    }
+}
